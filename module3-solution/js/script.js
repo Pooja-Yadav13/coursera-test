b4d1ef1eb2c231796ef86f8fcd9e73d123be1f51
ugamvpn <poojay7208@gmail.com>
2020-05-22 22:52:06 +0530

    hamburger menu changes

--- module3-solution/js/script.js
+++ module3-solution/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('.navbar-nav>li>a').on('click', function(){
+      $(".navbar-toggler-icon").toggleClass("toggle-color");
         $('.navbar-collapse').collapse('hide');
+
       });
+      $(".navbar-toggler").click(function(){
+        $(".navbar-toggler-icon").toggleClass("toggle-color");
+      });
+     
 })
