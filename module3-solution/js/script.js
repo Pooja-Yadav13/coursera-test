8f5e6383910f85982ae65eeb6411438ffcee6646
ugamvpn <poojay7208@gmail.com>
2020-05-25 19:27:50 +0530

    some style changes

--- module3-solution/js/script.js
+++ module3-solution/js/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
     $('.navbar-nav>li>a').on('click', function(){
       $(".navbar-toggler-icon").toggleClass("toggle-color");
+      $(".navbar-toggler").toggleClass("menu-border");
         $('.navbar-collapse').collapse('hide');
 
       });
       $(".navbar-toggler").click(function(){
+       
         $(".navbar-toggler-icon").toggleClass("toggle-color");
+        $(".navbar-toggler").toggleClass("menu-border");
+
       });
      
 })
